d9d474f116957a24cbe0fbb42ca6547adafdd0ea
Sam-Bartos <sambartos@gmail.com>
2019-02-25 12:28:15 +1300

    db get routes working

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getTodos} = require('../db/todos')
+const {getTodos, getTodosByPriority, getTodosByCategory, getTodosByCompleted} = require('../db/todos')
 
 //GET /api/v1/todos
 router.get('/', (req, res)=>{
@@ -14,6 +14,41 @@ router.get('/', (req, res)=>{
     })
 })
 
+router.get('/priority/:priority', (req, res)=>{
+    const priority = req.params.priority
+    getTodosByPriority(priority)
+    .then(todos => {
+        res.json(todos)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.setStatus(500).json({error: 'an error has occured'})
+    })
+})
+
+router.get('/category/:category', (req, res)=>{
+    const category = req.params.category
+    getTodosByCategory(category)
+    .then(todos => {
+        res.json(todos)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.setStatus(500).json({error: 'an error has occured'})
+    })
+})
+
+router.get('/completed/:completed', (req, res)=>{
+    const completed = req.params.completed
+    getTodosByCompleted(completed)
+    .then(todos => {
+        res.json(todos)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.setStatus(500).json({error: 'an error has occured'})
+    })
+})
 
 
 module.exports = router
