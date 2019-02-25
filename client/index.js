edbf47cdaf4bb856c46ca4afc4fba9547378475b
Sam-Bartos <sambartos@gmail.com>
2019-02-25 15:43:15 +1300

    react- hello world

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    ReactDOM.render(
+        <h1>Hello, Wellington!</h1>,
+        document.getElementById('app')
+    )
+})
+
