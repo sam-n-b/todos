943c65802479dc3e0b4a55bc3bde2e80ae525c3c
Sam-Bartos <sambartos@gmail.com>
2019-02-27 23:38:32 +1300

    change due_at to string

--- client/components/TodoForm.jsx
+++ client/components/TodoForm.jsx
@@ -41,8 +41,8 @@ return(
         <form onSubmit={this.handleSubmit}>
             <p>New Todo:<br/><input type='text' name ='task' onChange={this.handleChange} value={this.state.task}/></p>
             <p>Category:<br/><input type='text' name ='category'onChange={this.handleChange} value={this.state.category}/></p>
-            <p>Priority:<br/><input type='text' name ='priority'onChange={this.handleChange} value={this.state.priority}/></p>
-            <p>Due:<br/><input type='text' name='due_at'onChange={this.handleChange} value={this.state.due_at}/></p>
+            <p>Priority (1-5):<br/><input type='text' name ='priority'onChange={this.handleChange} value={this.state.priority}/></p>
+            <p>Due (YYYY-MM-DD):<br/><input type='text' name='due_at'onChange={this.handleChange} value={this.state.due_at}/></p>
             <p><input type='submit' value='Make Todo'/></p>
 
         </form>
