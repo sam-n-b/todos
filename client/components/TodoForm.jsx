9426ef6761c667973f79ed634d482213a60d4cb4
Sam-Bartos <sambartos@gmail.com>
2019-03-19 16:38:54 +1300

    date picker added in form

--- client/components/TodoForm.jsx
+++ client/components/TodoForm.jsx
@@ -54,7 +54,7 @@ return(
             <p>New Todo:<br/><input type='text' name ='task' onChange={this.handleChange} value={this.state.task}/></p>
             <p>Category:<br/><input type='text' name ='category'onChange={this.handleChange} value={this.state.category}/></p>
             <p>Priority (1-5):<br/><input type='text' name ='priority'onChange={this.handleChange} value={this.state.priority}/></p>
-            <p>Due (YYYY-MM-DD):<br/><input type='text' name='due_at'onChange={this.handleChange} value={this.state.due_at}/></p>
+            <p>Due (YYYY-MM-DD):<br/><input type='date' name='due_at'onChange={this.handleChange} value={this.state.due_at}/></p>
             <p><input type='submit' value='Make Todo'/></p>
 
         </form>
