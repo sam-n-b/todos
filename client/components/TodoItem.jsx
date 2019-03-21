fa321a61762071cfadd3185a2b99d56f5b9c8c2f
Sam-Bartos <sambartos@gmail.com>
2019-03-21 13:12:29 +1300

    completed todos image greyscale

--- client/components/TodoItem.jsx
+++ client/components/TodoItem.jsx
@@ -23,7 +23,7 @@ class TodoItem extends React.Component{
         <span>      Category: {this.props.todo.category}</span>
         <span>      Priority: {this.props.todo.priority}</span>
         <span>      Due: {this.props.todo.due_at}   </span>
-        <img className='priority-img' src={`/images/priority${this.props.todo.priority}.png`}/>
+        <img className={this.props.todo.is_complete?'priority-img priority-img-completed':'priority-img'} src={`/images/priority${this.props.todo.priority}.png`}/>
         <button onClick={this.handleClickComplete.bind(this)}>complete</button>
         <button onClick={this.handdleClickDelete.bind(this)}>remove</button>
         <br/>
