2bf281acb26f51c0416ec797d55463258bd75b91
Sam-Bartos <sambartos@gmail.com>
2019-03-25 15:46:11 +1300

    refactored TodoItem Component and fixed todoSearch dispatch bug

--- client/components/TodoItem.jsx
+++ client/components/TodoItem.jsx
@@ -7,28 +7,30 @@ class TodoItem extends React.Component{
         super(props)
     }
     handleClickComplete(e){
-        this.props.dispatch(toggleTodo(this.props.todo.id, !this.props.todo.is_complete))
-        this.props.dispatch(getTodos())
-        this.props.dispatch(getTodosSearch(this.props.todosSearchValue))
-        this.props.dispatch(getTodosPriority(this.props.todosPriorityValue))
+        const dispatch = this.props.dispatch
+        dispatch(toggleTodo(this.props.todo.id, !this.props.todo.is_complete))
+        dispatch(getTodos())
+        dispatch(getTodosPriority(this.props.todosPriorityValue))
+        this.props.todosSearchValue.length >0 && dispatch(getTodosSearch(this.props.todosSearchValue))
     }
-    handdleClickDelete(e){
-        this.props.dispatch(deleteTodo(this.props.todo.id))
-        this.props.dispatch(getTodos())
-        this.props.dispatch(getTodosSearch(this.props.todosSearchValue))
-        this.props.dispatch(getTodosPriority(this.props.todosPriorityValue))
+    handleClickDelete(e){
+        const dispatch = this.props.dispatch
+        dispatch(deleteTodo(this.props.todo.id))
+        dispatch(getTodos())
+        dispatch(getTodosPriority(this.props.todosPriorityValue))
+        this.props.todosSearchValue.length >0 && dispatch(getTodosSearch(this.props.todosSearchValue))
     }
    render(){
+       const todo = this.props.todo
     return(
-        
-        <div className={this.props.todo.is_complete?'todo-item completed':'todo-item'}>
-        <span><b>Todo: </b>{this.props.todo.task}</span>
-        <span>      <b>Category: </b>{this.props.todo.category}</span>
-        <span>      <b>Priority: </b>{this.props.todo.priority}</span>
-        <span>      <b>Due: </b>{this.props.todo.due_at}   </span>
-        <img className={this.props.todo.is_complete?'priority-img priority-img-completed':'priority-img'} src={`/images/priority${this.props.todo.priority}.png`}/>
+        <div className={todo.is_complete?'todo-item completed':'todo-item'}>
+        <span><b>Todo: </b>{todo.task}</span>
+        <span>      <b>Category: </b>{todo.category}</span>
+        <span>      <b>Priority: </b>{todo.priority}</span>
+        <span>      <b>Due: </b>{todo.due_at}   </span>
+        <img className={todo.is_complete?'priority-img priority-img-completed':'priority-img'} src={`/images/priority${todo.priority}.png`}/>
         <button onClick={this.handleClickComplete.bind(this)}>complete</button>
-        <button onClick={this.handdleClickDelete.bind(this)}>remove</button>
+        <button onClick={this.handleClickDelete.bind(this)}>remove</button>
         <br/>
         </div>
     )
